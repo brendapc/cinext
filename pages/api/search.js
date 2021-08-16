ea52bc5f8e121eb1fa27bf4947e3294cb105cc3d
Brenda Camara <brendacamara@outlook.com>
2021-08-16 15:07:53 -0300

    feat: search by movie

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,7 +1,7 @@
 import {apiBase, apiKey} from '../../lib/tmdb'
 
 export default async function trending(req, res) {
-    const response = await fetch(`${apiBase}/trending/movie/week?api_key=${apiKey}&language=pt-BR`)
+    const response = await fetch(`${apiBase}/search/movie/?api_key=${apiKey}&language=pt-BR&query=${req.query.value}`)
     const json = await response.json()
     
     res.status(200).json({ 
